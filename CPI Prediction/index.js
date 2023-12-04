5a11930045fdfa5e51b31626cdb6ee3b1f6abd9d
Jacob DeMuth <142455020+demuth-j@users.noreply.github.com>
2023-12-03 20:34:16 -0600

    Update index.js

--- CPI Prediction/index.js
+++ CPI Prediction/index.js
@@ -1,37 +1,225 @@
 async function runExample() {
 
-    var x = new Float32Array( 1, 11 )
+     var x = new Float32Array(1, 11)
 
-    var x = [];
+     var x = [];
 
-     x[0] = document.getElementById('box1').value;
-     x[1] = document.getElementById('box2').value;
-     x[2] = document.getElementById('box3').value;
-     x[3] = document.getElementById('box4').value;
-     x[4] = document.getElementById('box5').value;
-     x[5] = document.getElementById('box6').value;
-     x[6] = document.getElementById('box7').value;
-     x[7] = document.getElementById('box8').value;
-     x[8] = document.getElementById('box9').value;
-     x[9] = document.getElementById('box10').value;
-     x[10] = document.getElementById('box11').value;
+     x[0] = document.getElementById('CUUR0000SA0').value;
+     x[1] = document.getElementById('CUUR0000SAA').value;
+     x[2] = document.getElementById('CUUR0000SAC').value;
+     x[3] = document.getElementById('CUUR0000SAD').value;
+     x[4] = document.getElementById('CUUR0000SAE2').value;
+     x[5] = document.getElementById('CUUR0000SAF').value;
+     x[6] = document.getElementById('CUUR0000SAH').value;
+     x[7] = document.getElementById('CUUR0000SAM').value;
+     x[8] = document.getElementById('CUUR0000SAN').value;
+     x[9] = document.getElementById('CUUR0000SAR').value;
+     x[10] = document.getElementById('CUUR0000SAS').value;
+     x[11] = document.getElementById('CUUR0000SAT').value;
+     x[12] = document.getElementById('CUUR0000SEEE').value;
+     x[13] = document.getElementById('CUUR0000SEHG').value;
+     x[14] = document.getElementById('CUUR0000SEHJ').value;
+     x[15] = document.getElementById('CUUR0000SEHK').value;
+     x[16] = document.getElementById('CUUR0000SETA').value;
 
-    let tensorX = new ort.Tensor('float32', x, [1, 11] );
-    let feeds = {float_input: tensorX};
+     x[17] = document.getElementById('CUUR0100SA0').value;
+     x[18] = document.getElementById('CUUR0100SA0E').value;
+     x[19] = document.getElementById('CUUR0100SAA').value;
+     x[20] = document.getElementById('CUUR0100SAC').value;
+     x[21] = document.getElementById('CUUR0100SAD').value;
+     x[22] = document.getElementById('CUUR0100SAF').value;
+     x[23] = document.getElementById('CUUR0100SAH').value;
+     x[24] = document.getElementById('CUUR0100SAM').value;
+     x[25] = document.getElementById('CUUR0100SAN').value;
+     x[26] = document.getElementById('CUUR0100SAR').value;
+     x[27] = document.getElementById('CUUR0100SAS').value;
+     x[28] = document.getElementById('CUUR0100SAT').value;
+     x[29] = document.getElementById('CUUR0100SETA').value;
 
-    let session = await ort.InferenceSession.create('xgboost_WineQuality_ort.onnx');
-    
-   let result = await session.run(feeds);
-   let outputData = result.variable.data;
+     x[30] = document.getElementById('CUUR0110SA0').value;
+     x[31] = document.getElementById('CUUR0110SA0E').value;
+     x[32] = document.getElementById('CUUR0110SAA').value;
+     x[33] = document.getElementById('CUUR0110SAC').value;
+     x[34] = document.getElementById('CUUR0110SAD').value;
+     x[35] = document.getElementById('CUUR0110SAF').value;
+     x[36] = document.getElementById('CUUR0110SAH').value;
+     x[37] = document.getElementById('CUUR0110SAM').value;
+     x[38] = document.getElementById('CUUR0110SAN').value;
+     x[39] = document.getElementById('CUUR0110SAR').value;
+     x[40] = document.getElementById('CUUR0110SAS').value;
+     x[41] = document.getElementById('CUUR0110SAT').value;
+     x[42] = document.getElementById('CUUR0110SETA').value;
 
-  outputData = parseFloat(outputData).toFixed(2)
+     x[43] = document.getElementById('CUUR0120SA0').value;
+     x[44] = document.getElementById('CUUR0120SA0E').value;
+     x[45] = document.getElementById('CUUR0120SAA').value;
+     x[46] = document.getElementById('CUUR0120SAC').value;
+     x[47] = document.getElementById('CUUR0120SAD').value;
+     x[48] = document.getElementById('CUUR0120SAF').value;
+     x[49] = document.getElementById('CUUR0120SAH').value;
+     x[50] = document.getElementById('CUUR0120SAM').value;
+     x[51] = document.getElementById('CUUR0120SAN').value;
+     x[52] = document.getElementById('CUUR0120SAR').value;
+     x[53] = document.getElementById('CUUR0120SAS').value;
+     x[54] = document.getElementById('CUUR0120SAT').value;
+     x[55] = document.getElementById('CUUR0120SETA').value;
 
-   let predictions = document.getElementById('predictions');
+     x[56] = document.getElementById('CUUR0200SA0').value;
+     x[57] = document.getElementById('CUUR0200SA0E').value;
+     x[58] = document.getElementById('CUUR0200SAA').value;
+     x[59] = document.getElementById('CUUR0200SAC').value;
+     x[60] = document.getElementById('CUUR0200SAD').value;
+     x[61] = document.getElementById('CUUR0200SAF').value;
+     x[62] = document.getElementById('CUUR0200SAH').value;
+     x[63] = document.getElementById('CUUR0200SAM').value;
+     x[64] = document.getElementById('CUUR0200SAN').value;
+     x[65] = document.getElementById('CUUR0200SAR').value;
+     x[66] = document.getElementById('CUUR0200SAS').value;
+     x[67] = document.getElementById('CUUR0200SAT').value;
+     x[68] = document.getElementById('CUUR0200SETA').value;
 
+     x[69] = document.getElementById('CUUR0230SA0').value;
+     x[70] = document.getElementById('CUUR0230SA0E').value;
+     x[71] = document.getElementById('CUUR0230SAA').value;
+     x[72] = document.getElementById('CUUR0230SAC').value;
+     x[73] = document.getElementById('CUUR0230SAD').value;
+     x[74] = document.getElementById('CUUR0230SAF').value;
+     x[75] = document.getElementById('CUUR0230SAH').value;
+     x[76] = document.getElementById('CUUR0230SAM').value;
+     x[77] = document.getElementById('CUUR0230SAN').value;
+     x[78] = document.getElementById('CUUR0230SAR').value;
+     x[79] = document.getElementById('CUUR0230SAS').value;
+     x[80] = document.getElementById('CUUR0230SAT').value;
+     x[81] = document.getElementById('CUUR0230SETA').value;
+
+     x[82] = document.getElementById('CUUR0240SA0').value;
+     x[83] = document.getElementById('CUUR0240SA0E').value;
+     x[84] = document.getElementById('CUUR0240SAA').value;
+     x[85] = document.getElementById('CUUR0240SAC').value;
+     x[86] = document.getElementById('CUUR0240SAD').value;
+     x[87] = document.getElementById('CUUR0240SAF').value;
+     x[88] = document.getElementById('CUUR0240SAH').value;
+     x[89] = document.getElementById('CUUR0240SAM').value;
+     x[90] = document.getElementById('CUUR0240SAN').value;
+     x[91] = document.getElementById('CUUR0240SAR').value;
+     x[92] = document.getElementById('CUUR0240SAS').value;
+     x[93] = document.getElementById('CUUR0240SAT').value;
+     x[94] = document.getElementById('CUUR0240SETA').value;
+
+     x[95] = document.getElementById('CUUR0300SA0').value;
+     x[96] = document.getElementById('CUUR0300SA0E').value;
+     x[97] = document.getElementById('CUUR0300SAA').value;
+     x[98] = document.getElementById('CUUR0300SAC').value;
+     x[99] = document.getElementById('CUUR0300SAD').value;
+     x[100] = document.getElementById('CUUR0300SAF').value;
+     x[101] = document.getElementById('CUUR0300SAH').value;
+     x[102] = document.getElementById('CUUR0300SAM').value;
+     x[103] = document.getElementById('CUUR0300SAN').value;
+     x[104] = document.getElementById('CUUR0300SAR').value;
+     x[105] = document.getElementById('CUUR0300SAS').value;
+     x[106] = document.getElementById('CUUR0300SAT').value;
+     x[107] = document.getElementById('CUUR0300SETA').value;
+
+     x[108] = document.getElementById('CUUR0350SA0').value;
+     x[109] = document.getElementById('CUUR0350SA0E').value;
+     x[110] = document.getElementById('CUUR0350SAA').value;
+     x[111] = document.getElementById('CUUR0350SAC').value;
+     x[112] = document.getElementById('CUUR0350SAD').value;
+     x[113] = document.getElementById('CUUR0350SAF').value;
+     x[114] = document.getElementById('CUUR0350SAH').value;
+     x[115] = document.getElementById('CUUR0350SAM').value;
+     x[116] = document.getElementById('CUUR0350SAN').value;
+     x[117] = document.getElementById('CUUR0350SAR').value;
+     x[118] = document.getElementById('CUUR0350SAS').value;
+     x[119] = document.getElementById('CUUR0350SAT').value;
+     x[120] = document.getElementById('CUUR0350SETA').value;
+
+     x[121] = document.getElementById('CUUR0360SA0').value;
+     x[122] = document.getElementById('CUUR0360SA0E').value;
+     x[123] = document.getElementById('CUUR0360SAA').value;
+     x[124] = document.getElementById('CUUR0360SAC').value;
+     x[125] = document.getElementById('CUUR0360SAD').value;
+     x[126] = document.getElementById('CUUR0360SAF').value;
+     x[127] = document.getElementById('CUUR0360SAH').value;
+     x[128] = document.getElementById('CUUR0360SAM').value;
+     x[129] = document.getElementById('CUUR0360SAN').value;
+     x[130] = document.getElementById('CUUR0360SAR').value;
+     x[131] = document.getElementById('CUUR0360SAS').value;
+     x[132] = document.getElementById('CUUR0360SAT').value;
+     x[133] = document.getElementById('CUUR0360SETA').value;
+
+     x[134] = document.getElementById('CUUR0370SA0').value;
+     x[135] = document.getElementById('CUUR0370SA0E').value;
+     x[136] = document.getElementById('CUUR0370SAA').value;
+     x[137] = document.getElementById('CUUR0370SAC').value;
+     x[138] = document.getElementById('CUUR0370SAD').value;
+     x[139] = document.getElementById('CUUR0370SAF').value;
+     x[140] = document.getElementById('CUUR0370SAH').value;
+     x[141] = document.getElementById('CUUR0370SAM').value;
+     x[142] = document.getElementById('CUUR0370SAN').value;
+     x[143] = document.getElementById('CUUR0370SAR').value;
+     x[144] = document.getElementById('CUUR0370SAS').value;
+     x[145] = document.getElementById('CUUR0370SAT').value;
+     x[146] = document.getElementById('CUUR0370SETA').value;
+
+     x[147] = document.getElementById('CUUR0400SA0').value;
+     x[148] = document.getElementById('CUUR0400SA0E').value;
+     x[149] = document.getElementById('CUUR0400SAA').value;
+     x[150] = document.getElementById('CUUR0400SAC').value;
+     x[151] = document.getElementById('CUUR0400SAD').value;
+     x[152] = document.getElementById('CUUR0400SAF').value;
+     x[153] = document.getElementById('CUUR0400SAH').value;
+     x[154] = document.getElementById('CUUR0400SAM').value;
+     x[155] = document.getElementById('CUUR0400SAN').value;
+     x[156] = document.getElementById('CUUR0400SAR').value;
+     x[157] = document.getElementById('CUUR0400SAS').value;
+     x[158] = document.getElementById('CUUR0400SAT').value;
+     x[159] = document.getElementById('CUUR0400SETA').value;
+
+     x[160] = document.getElementById('CUUR0480SA0').value;
+     x[161] = document.getElementById('CUUR0480SA0E').value;
+     x[162] = document.getElementById('CUUR0480SAA').value;
+     x[163] = document.getElementById('CUUR0480SAC').value;
+     x[164] = document.getElementById('CUUR0480SAD').value;
+     x[165] = document.getElementById('CUUR0480SAF').value;
+     x[166] = document.getElementById('CUUR0480SAH').value;
+     x[167] = document.getElementById('CUUR0480SAM').value;
+     x[168] = document.getElementById('CUUR0480SAN').value;
+     x[169] = document.getElementById('CUUR0480SAR').value;
+     x[170] = document.getElementById('CUUR0480SAS').value;
+     x[171] = document.getElementById('CUUR0480SAT').value;
+     x[172] = document.getElementById('CUUR0480SETA').value;
+
+     x[173] = document.getElementById('CUUR0490SA0').value;
+     x[174] = document.getElementById('CUUR0490SA0E').value;
+     x[175] = document.getElementById('CUUR0490SAA').value;
+     x[176] = document.getElementById('CUUR0490SAC').value;
+     x[177] = document.getElementById('CUUR0490SAD').value;
+     x[178] = document.getElementById('CUUR0490SAF').value;
+     x[179] = document.getElementById('CUUR0490SAH').value;
+     x[180] = document.getElementById('CUUR0490SAM').value;
+     x[181] = document.getElementById('CUUR0490SAN').value;
+     x[182] = document.getElementById('CUUR0490SAR').value;
+     x[183] = document.getElementById('CUUR0490SAS').value;
+     x[184] = document.getElementById('CUUR0490SAT').value;
+     x[185] = document.getElementById('CUUR0490SETA').value;
+
+     let tensorX = new ort.Tensor('float32', x, [1, 186]);
+     let feeds = {float_input: tensorX};
+
+     let session = await ort.InferenceSession.create('xgboost_WineQuality_ort.onnx');
+
+     let result = await session.run(feeds);
+    let outputData = result.variable.data;
+
+    outputData = parseFloat(outputData).toFixed(2)
+
+    let predictions = document.getElementById('predictions');
   predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
    <table>
      <tr>
        <td>  Rating of Wine Quality  </td>
-       <td id="td0">  ${outputData}  </td>
+       <td id="td0">  ${outputData.data[0].toFixed(2)}  </td>
      </tr>
   </table>`;
+}
